5722baf77ba81b557f749a051f532ba43cb4713d
Emily Stephen <estephen@bu.edu>
2014-07-12 14:22:00 -0400

    Initial commit

---
+++ spectra_d3.js
@@ -0,0 +1,25 @@
+
+var margin = {top: 20, right: 10, bottom: 20, left: 10};
+var width = document.getElementById("Ch1Panel").offsetWidth - margin.left - margin.right,
+    height = document.getElementById("Ch1Panel").offsetWidth*4/5 - margin.top - margin.bottom;
+
+
+svgCh1 = d3.select("#Ch1Panel").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svgCh2 = d3.select("#Ch2Panel").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svgCoh = d3.select("#CoherencePanel").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Load Channel 1 data, make heatmap
