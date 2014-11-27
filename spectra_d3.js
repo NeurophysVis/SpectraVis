ede6dde57f758a4fa837c6da3f44b225005d3d5b
Eric Denovellis <edeno@Erics-MacBook-Air.local>
2014-11-27 15:28:21 -0500

    Loading and scales

--- spectra_d3.js
+++ spectra_d3.js
@@ -1,4 +1,3 @@
-// This is a test comment for github learning
 var margin = {top: 20, right: 10, bottom: 20, left: 10};
 var width = document.getElementById("Ch1Panel").offsetWidth - margin.left - margin.right,
     height = document.getElementById("Ch1Panel").offsetWidth*4/5 - margin.top - margin.bottom;
@@ -22,4 +21,84 @@ svgCoh = d3.select("#CoherencePanel").append("svg")
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Load Channel 1 data, make heatmap
+// Load data
+var curSubject = "SIM03_B0.00T0.63",
+    curCh1 = "1",
+    curCh2 = "2";
+
+var spectCh1_file = "spectrogram_" + curSubject + "_" + "C" + curCh1 + ".json",
+    spectCh2_file = "spectrogram_" + curSubject + "_" + "C" + curCh2 + ".json",
+    coh_file = "coherogram_" + curSubject + "_" + "C" + curCh1 + "_C" + curCh2 + ".json";
+
+// Wait until files are loaded before drawing heatmaps
+queue()
+    .defer(d3.json, "DATA/" + spectCh1_file)
+    .defer(d3.json, "DATA/" + spectCh2_file)
+    .defer(d3.json, "DATA/" + coh_file)
+    .await(display);
+
+// Draws heatmaps
+function display(isError, spect1_data, spect2_data, coh_data) {
+
+  var timeScale, freqScale, powerScale, cohScale;
+
+  setupScales();
+
+  drawSpect(svgCh1, spect1_data);
+  drawCoh(svgCoh, coh_data);
+  drawSpect(svgCh2, spect2_data);
+
+  function setupScales() {
+    var powerMin, powerMax, cohMax, cohMin;
+
+    powerMin = d3.min(
+      [d3.min(spect1_data.data, function(d) {
+        return d3.min(d, function(e) {return e;});
+      }),
+      d3.min(spect2_data.data, function(d) {
+        return d3.min(d, function(e) {return e;});
+      })]
+    );
+
+    powerMax = d3.max(
+      [d3.max(spect1_data.data, function(d) {
+        return d3.max(d, function(e) {return e;});
+      }),
+      d3.max(spect2_data.data, function(d) {
+        return d3.max(d, function(e) {return e;});
+      })]
+    );
+
+    cohMin = d3.min(coh_data.data, function(d) {
+      return d3.min(d, function(e) {return e;});
+    });
+
+    cohMax = d3.max(coh_data.data, function(d) {
+      return d3.max(d, function(e) {return e;});
+    });
+
+    timeScale = d3.scale.linear()
+      .domain(d3.extent(spect1_data.tax))
+      .range([width, 0]);
+
+    freqScale = d3.scale.linear()
+      .domain(d3.extent(spect1_data.fax))
+      .range([0, height]);
+
+    powerScale = d3.scale.linear()
+      .domain([powerMin, powerMax]);
+
+    cohScale = d3.scale.linear()
+      .domain([cohMin, cohMax]);
+
+
+
+  }
+  function drawSpect(spectGroup, spect_data) {
+
+  }
+  function drawCoh(cohGroup, coh_data) {
+
+  }
+
+}
