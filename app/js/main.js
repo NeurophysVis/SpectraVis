56e302b8672323917275f7aafb40bc3acf48d2ec
edeno <edeno@bu.edu>
2015-11-30 14:25:25 -0500

    Make help overlay code more clear

--- app/js/main.js
+++ app/js/main.js
@@ -101,9 +101,20 @@
     .append('g')
     .attr('transform', 'translate(' + 40 + ',' + 40 + ')');
 
-  // Set up tool tip
-  var toolTip = d3.select('#overlay');
-  toolTip.selectAll('#close')
+  // Set up help overlay
+  var overlay = d3.select('#overlay');
+  var helpButton = d3.select('button#help-button');
+  overlay.selectAll('.close')
+    .on('click', function() {
+      overlay.style('display', 'none');
+    });
+
+  helpButton
+    .on('click', function() {
+      overlay
+        .style('display', 'block');
+    });
+
   // Set up permalink button
   var permalink = d3.select('#permalink');
   var linkButton = d3.select('button#link');
